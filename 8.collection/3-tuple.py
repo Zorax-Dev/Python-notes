c0de96434b560e89138a655cd5b1a94480a54159
Sarvesh-Dixit-dev <249896712+Sarvesh-Dixit-dev@users.noreply.github.com>
2026-05-16 17:33:30 +0530

    :)

---
+++ 8.collection/3-tuple.py
@@ -0,0 +1,12 @@
+# Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+
+fruits = ("apple", "orange", "banana", "coconut")
+
+print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# print(fruits.index("apple"))
+
+print(fruits.count("coconut"))
